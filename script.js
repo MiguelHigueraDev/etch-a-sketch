8db9efca2a0fed7104f728b1b397a9e319a84b13
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2023-10-30 04:47:14 -0300

    Add lighten mode

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const HSL_REGEX = /(.*?)hsl\((\d+),(\d+)%,(\d+)%\)/;
+const RGB_REGEX = /rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/;
+
 
 // Set grid size programmatically
 const gridContainer = document.querySelector('#grid');
@@ -85,13 +88,84 @@ function changeColor(e) {
     if (e.type === 'mouseover' && !mouseDown) return;
     if(mode === "color") e.target.style.backgroundColor = brushColor;
     if(mode === "eraser") e.target.style.backgroundColor = backgroundColor;
-    if(mode === "rainbow") {
-        const hue = Math.floor(Math.random() * 360);
-        const saturation = Math.floor(Math.random() * 100);
-        const lightness = Math.floor(Math.random() * 100);
-        e.target.style.backgroundColor = `hsl(${hue},${saturation}%,${lightness}%)`;
+    if(mode === "rainbow") e.target.style.backgroundColor = getRandomHslColor();
+    if(mode === "lighten") {
+        e.target.style.backgroundColor = lighten(e.target.style.backgroundColor);
     }
 }
+
+function getRandomHslColor() {
+    const hue = Math.floor(Math.random() * 360);
+    const saturation = Math.floor(Math.random() * 100);
+    const lightness = Math.floor(Math.random() * 100);
+    return `hsl(${hue},${saturation}%,${lightness}%)`;
+}
+
+function lighten(rgb) {
+    const hsl = getHslFromRgb(rgb);
+    const hslArray = HSL_REGEX.exec(hsl);
+    let hue = hslArray[2];
+    let saturation = hslArray[3];
+    let lightness = Number(hslArray[4]);
+    lightness += 7;
+    return `hsl(${hue},${saturation}%,${lightness}%)`;
+}
+
+
+function getHslFromRgb(rgb) {
+
+    // Extract RGB channels from RGB string
+    const rgbArray = RGB_REGEX.exec(rgb);
+    let r = rgbArray[1];
+    let g = rgbArray[2];
+    let b = rgbArray[3];
+
+    // Credit: https://css-tricks.com/converting-color-spaces-in-javascript/
+    // Make r, g, and b fractions of 1
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    // Find greatest and smallest channel values
+    let cmin = Math.min(r,g,b),
+    cmax = Math.max(r,g,b),
+    delta = cmax - cmin,
+    h = 0,
+    s = 0,
+    l = 0;
+
+    // Calculate hue
+    // No difference
+    if (delta == 0)
+    h = 0;
+    // Red is max
+    else if (cmax == r)
+    h = ((g - b) / delta) % 6;
+    // Green is max
+    else if (cmax == g)
+    h = (b - r) / delta + 2;
+    // Blue is max
+    else
+    h = (r - g) / delta + 4;
+
+    h = Math.round(h * 60);
+    
+    // Make negative hues positive behind 360°
+    if (h < 0)
+    h += 360;
+    // Calculate lightness
+    l = (cmax + cmin) / 2;
+
+    // Calculate saturation
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    
+    // Multiply l and s by 100
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+
+    return `hsl(${Math.round(h)},${Math.round(s)}%,${Math.round(l)}%)`;
+}
+
 
 generateGrid();
 
