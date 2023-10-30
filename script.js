38aacc87d6f5a21e6bda859aca055e68e3774258
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2023-10-30 04:01:23 -0300

    Finish basic color mode.

--- script.js
+++ script.js
@@ -1,37 +1,56 @@
 
 // Set grid size programmatically
 const gridContainer = document.querySelector('#grid');
-let gridSize = 16;
-const GRID_HEIGHT = 1200;
-const GRID_WIDTH = 1200;
+const GRID_HEIGHT = 800;
+const GRID_WIDTH = 800;
 gridContainer.style.width = GRID_WIDTH + "px";
 gridContainer.style.height = GRID_HEIGHT + "px";
 
-// Handle range UI change
-const rangeInput = document.querySelector("#grid-size");
-const rangeText = document.querySelector("#grid-size-number");
-rangeInput.addEventListener('input', (e) => {
+// Handle grid size UI change
+let gridSize = 16;
+const gridSizeInput = document.querySelector("#grid-size");
+const gridSizeText = document.querySelector("#grid-size-number");
+gridSizeInput.addEventListener('input', (e) => {
     gridSize = e.target.value;
     generateGrid();
-    rangeText.textContent = `(${gridSize}x${gridSize})`;
+    gridSizeText.textContent = `(${gridSize}x${gridSize})`;
+});
+
+// Handle color UI change
+let backgroundColor = "#FFFFFF";
+let brushColor = "#000000";
+
+const backgroundColorInput = document.querySelector("#background-color");
+backgroundColorInput.addEventListener('input', (e) => {
+    backgroundColor = e.target.value;
+});
+
+const brushColorInput = document.querySelector("#brush-color");
+brushColorInput.addEventListener('input', (e) => {
+    brushColor = e.target.value;
 });
 
 const generateGridButton = document.querySelector("#generate-grid");
-generateGridButton.addEventListener('click', (e) => {
-    generateGrid(gridSize);
-})
+generateGridButton.addEventListener('click', () => {
+    generateGrid();
+});
+
+// Handle mode change
 
 function generateGrid() {
-    // delete old grid first
     document.querySelectorAll('.square').forEach(e => e.remove());
     for(let i = 0; i < gridSize * gridSize; i++) {
         const square = document.createElement('div');
         square.classList = "square";
         square.style.width = GRID_WIDTH / gridSize + "px";
         square.style.height = GRID_HEIGHT / gridSize + "px";
+        square.style.backgroundColor = backgroundColor;
         square.id = i;
+        square.addEventListener('mouseover', changeColor)
+        square.addEventListener('mousedown', changeColor)
         gridContainer.appendChild(square);
     }
+
 }
 
 // Handle mouseover and mouseout, to add (and remove) trailing effect
@@ -47,7 +66,18 @@ gridContainer.addEventListener('mouseout', (e) => {
     }, trailDuration);
 });
 
-generateGrid();
+
+// Handle drawing on the grid
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) return;
+    e.target.style.backgroundColor = brushColor;
+}
+
+// Generate grid without waiting
+generateGrid(0);
 
 // TODO:
 /*
